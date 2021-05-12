fec24246dff7ee07cb7961413e255a50faf5ca82
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-05-12 23:48:16 +0900

    Update chsh with sudo

--- chsh.sh
+++ chsh.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -e
 
-# ログインシェルをzshに変更する
-chsh --shell `which zsh` `whoami`
+# パスワードを $password 変数に格納する
+printf "password: "
+read password
+
+# ユーザ名を $username 変数に格納する
+username=`whoami`
+
+# 「ログインシェルをzshに変更する」を 格納した $password を使って sudo で実行
+echo "$password" | sudo -S chsh --shell `which zsh` "$username"
